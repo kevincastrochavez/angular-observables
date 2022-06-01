fd54034a76e34bad3b57792bf2ff4d78725402e4
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-01 14:42:59 -0600

    learned about errors and completion in observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { interval, Subscription, Observable } from "rxjs";
+import { Subscription, Observable } from "rxjs";
 
 @Component({
   selector: "app-home",
@@ -19,13 +19,28 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3"));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
-      console.log(data);
-    });
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log("Completed");
+      }
+    );
   }
 
   ngOnDestroy() {
